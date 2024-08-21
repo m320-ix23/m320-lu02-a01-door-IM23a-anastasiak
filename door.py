6a8b71a852f0ad0fae6d7328562bc734a3c07096
IM23a-anastasiak <anastasiak@bzz,ch>
2024-08-21 16:00:50 +0200

    korrigiert

--- door.py
+++ door.py
@@ -1,14 +1,21 @@
-class Door:
-    """
+"""
     Diese Klasse beschreibt eine Türe mit der Eigenschaft color (Farbe) und den Zuständen
     door_is_open (für geöffnete Türe) sowie door_is_locked (für verriegelte Türe).
     Die Türe überwacht die beiden Zustände und verhindert so Aktionen, die nicht möglich sind.
     Das verriegeln selber delegiert die Türe an ein Objekt vom Typ Door_lock (Türschloss).
     """
+class Door:
+    """
+        Diese Klasse beschreibt eine Türe mit der Eigenschaft color (Farbe) und den Zuständen
+        door_is_open (für geöffnete Türe) sowie door_is_locked (für verriegelte Türe).
+        Die Türe überwacht die beiden Zustände und verhindert so Aktionen, die nicht möglich sind.
+        Das verriegeln selber delegiert die Türe an ein Objekt vom Typ Door_lock (Türschloss).
+        """
 
-    #Hallo Ich Bob!
+    # Hallo Ich Bob!
     # Mit dem Keyword def wird eine Funktion bzw. eben ein Konstruktor deklariert.
-    # Der Konstruktor trägt IMMER den Namen __init__ und weist als ersten Parameter den Wert self auf.
+    # Der Konstruktor trägt IMMER den Namen __init__ und weist als ersten Parameter
+    # den Wert self auf.
     # Danach folgen die Übergabeparameter, deren Werte dann den Attributen zugewiesen werden.
     # Attribute können aber auch mit einem fixen Wert initialisiert werden.
     # Konstruktoren werden als Erstes im Programm angeschrieben.
@@ -33,13 +40,14 @@ class Door:
         Methode für das öffnen der Türe.
         Das ist aber nur möglich, wenn die Türe nicht verriegelt ist.
         """
-        if self._door_is_locked == False:
+        if self._door_is_locked is False:
             self._door_is_open = True
 
     def close_the_door(self):
         """
         Methode für das schliessen der Türe.
-        Das geht immer, auch wenn die Türe schon geschlossen oder verriegelt ist. Der Zustand ändert dann nämlich nicht.
+        Das geht immer, auch wenn die Türe schon geschlossen oder verriegelt ist.
+        Der Zustand ändert dann nämlich nicht.
         """
         self._door_is_open = False
 
@@ -49,7 +57,7 @@ class Door:
         Das ist nur möglich, wenn die Türe nicht offen ist.
         Für das verriegeln ist aber das Türschloss zuständig. Es weiss wie das geht.
         """
-        if self._door_is_open == False:
+        if self._door_is_open is False:
             self._door_is_locked = self._the_door_lock.lock()
 
     def unlock_the_door(self):
@@ -105,9 +113,9 @@ class Door:
         self._color = new_color
 
 
-"""
-nur für die korrekte Übersetzung und Ausführung 
-"""
+
+ # nur für die korrekte Übersetzung und Ausführung
+
 
 
 class DoorLock:
@@ -119,9 +127,17 @@ class DoorLock:
         print("ein Schloss erzeugt")
 
     def lock(self):
+        """
+
+        :return:
+        """
         return True
 
     def unlock(self):
+        """
+
+        :return:
+        """
         return False
 
 
@@ -131,6 +147,6 @@ if __name__ == "__main__":
     the_door_lock = DoorLock()
     the_door = Door(the_door_lock, "grün")
     the_door.test()
-    print("-- Türe jetzt öffnen")
+    print("-- Türe jetzt öffnen\n")
     the_door.open_the_door()
     the_door.test()
